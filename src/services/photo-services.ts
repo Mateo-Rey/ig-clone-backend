023a2afd211d0ce248e8c8e09b9ab2b873489904
Mateo-Rey <jrey3darts@gmail.com>
2022-08-17 11:58:41 -0400

    post and get route

--- src/services/photo-services.ts
+++ src/services/photo-services.ts
@@ -1,16 +1,48 @@
 import { Photo } from "../models/photo";
-import {db} from '../db/db'
+import { db } from "../db/db";
+import { ObjectId } from "mongodb";
 interface PhotoServices {
-    updateLikes(id: string, inc: number): Promise<Photo>
-    createPhoto(photo: Photo): Promise<string>
-    createComment(id: string, comment: string): Promise<Photo>
-    getAllPhotos(): Promise<Photo[]>
+    updateLikes(id: string, inc: number): Promise<Photo>;
+  //   createComment(id: string, comment: string): Promise<Photo>;
+  createPhoto(photo: Photo): Promise<string>;
+  getAllPhotos(): Promise<Photo[]>;
 }
 
-const photoCollections = db.collection<Photo>('photos')
+const photoCollections = db.collection<Photo>("photos");
 
-export const getAllPhotos = async ( ): Promise <Photo[]> => {
-    const photos = await photoCollections.find().toArray()
+export const getAllPhotos = async (): Promise<Photo[]> => {
+  const photos = await photoCollections.find().toArray();
 
-    return photos;
-}
+  return photos;
+};
+
+const createPhoto = async (photo: Photo): Promise<string> => {
+  try {
+    const res = await photoCollections.insertOne(photo);
+    return res.insertedId.toString();
+  } catch (err) {
+    console.log(err);
+    return "Something went wrong";
+  }
+};
+
+export const updateLikes = async (
+  id: string,
+  inc: number = 1
+): Promise<Photo> => {
+  const res = await photoCollections.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $inc: { likes: inc } }
+  );
+
+  const updatedPhoto = res.value as Photo
+//   updatedPhoto.likes = inc
+
+  return updatedPhoto
+};
+
+// export const createComment = async (id: string, comment: string): Promise<Photo> => {
+
+// }
+
+export const photoServices: PhotoServices = { getAllPhotos, updateLikes, createPhoto };
